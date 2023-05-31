ac390a5fc15bb7a7acb97ea209252390fb08bc1a
psoper1 <psoper0604@gmail.com>
2023-05-31 18:22:31 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const apiChamps = 'http://127.0.0.1:8000/champs/';
+  // const imageURL = `${'http://127.0.0.1:8000/'}${champ.image}`;
+
+  useEffect(() => {
+    fetch(apiChamps)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }, []);  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Use the `data` state in your component */}
+      {data.map(item => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <p>Age: {item.age}</p>
+          <img src={item.image} alt='champ-icon'/>
+          {/* Render other data properties */}
+        </div>
+      ))}
     </div>
   );
 }
