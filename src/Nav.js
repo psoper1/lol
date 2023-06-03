cb9651e52316aa1e087780c52a319aeb73822cb8
psoper1 <psoper0604@gmail.com>
2023-06-03 12:52:03 -0400

    Styling Nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,39 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Nav() {
+    const location = useLocation();
+
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-custom">
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link to='/' className='text-color nav-link'>Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to='/champ-list' className='text-color nav-link'>Full Champ List</Link>
-                            </li>
-                        </ul>
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-custom">
+            <div className="container-fluid">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav d-flex justify-content-between w-50">
+                        <li className={`nav-item col ${location.pathname === '/' ? 'active' : ''}`}>
+                            <NavLink to="/" className="nav-link">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className={`nav-item col ${location.pathname === '/champ-list' ? 'active' : ''}`}>
+                            <NavLink to="/champ-list" className="nav-link">
+                                Full Champ List
+                            </NavLink>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
+
     )
 }
 
