9b855daf4ce55ca726c520be58cb326f48f95145
psoper1 <psoper0604@gmail.com>
2023-06-03 13:11:26 -0400

    re

--- src/SelectedChamp.js
+++ src/SelectedChamp.js
@@ -1,13 +1,26 @@
 import Nav from "./Nav";
+import { useRef } from 'react';
 
-function SelectedChamp({champ}) {
+function SelectedChamp({ champ }) {
+    const detailsSectionRef = useRef(null);
+
+    const scrollToDetailsSection = () => {
+        detailsSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
-        <>
-        <Nav />
-        <h1 className='text-center champname text-color'>{champ.name}</h1>
-        <img src={champ?.splash} className="img-fluid splash" alt='splash'/>
-        </>
-    )
+        <div>
+            <section className="page-section">
+                <Nav />
+                <h1 className='text-center champname text-color'>{champ.name}</h1>
+                <img src={champ?.splash} className="img-fluid splash" alt='splash' />
+                <div className="scroll-arrow text-color" onClick={scrollToDetailsSection}>Details</div>
+            </section>
+            <section className="page-section details-section" ref={detailsSectionRef}>
+                <p>I am text in the next section</p>
+            </section>
+        </div>
+    );
 }
 
 export default SelectedChamp;
