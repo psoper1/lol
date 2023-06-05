ef4c5a7dfcc4d537c3e7f9682a960ae363cb9d3d
psoper1 <psoper0604@gmail.com>
2023-06-05 12:23:01 -0400

    edit

--- src/ChampList.js
+++ src/ChampList.js
@@ -61,7 +61,7 @@ function ChampList({ champ, setChamp, data, setData }) {
             <video autoPlay muted loop className="background-video">
                 <source src="https://assets.contentstack.io/v3/assets/blt2ac872571a60ee02/bltc3128a843ac2ef28/618d752b6407fe7f991e9915/background-video-d-01.mp4" type="video/mp4" />
             </video>
-            <div className='container lane icons'>
+            <div className='container lane-icons'>
                 <img
                     src={fill}
                     alt='fill'
