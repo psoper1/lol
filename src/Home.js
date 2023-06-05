3071258c46f57e6654c9cfba3ae45df108a012a6
psoper1 <psoper0604@gmail.com>
2023-06-05 08:59:11 -0400

    added images for champion sorting

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,12 @@ function Home() {
 
     return (
         <>
-        <Nav />
-        <div className='text-center text-white'>REEEEEEEEEEEEEEEEE</div>
+            <Nav />
+            <div className="container">
+                <div class="form-outline">
+                    <input type="search" id="summoner-search" class="form-control text-center1" placeholder="Search for Summoner" aria-label="Search" />
+                </div>
+            </div>
         </>
     )
 }
