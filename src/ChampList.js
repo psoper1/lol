f0b42663b45f5d209995f0cc5eb57e7018324d89
psoper1 <psoper0604@gmail.com>
2023-06-05 08:59:30 -0400

    added images for champion sorting

--- src/ChampList.js
+++ src/ChampList.js
@@ -2,6 +2,12 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import Nav from './Nav';
+import top from './imgs/lanes/Top_icon.png';
+import jungle from './imgs/lanes/Jungle_icon.png';
+import mid from './imgs/lanes/Middle_icon.png';
+import bot from './imgs/lanes/Bottom_icon.png';
+import support from './imgs/lanes/Support_icon.png';
+import fill from './imgs/lanes/fill.png';
 
 function ChampList({ champ, setChamp, data, setData }) {
 
@@ -46,6 +52,14 @@ function ChampList({ champ, setChamp, data, setData }) {
             <video autoPlay muted loop className="background-video">
                 <source src="https://assets.contentstack.io/v3/assets/blt2ac872571a60ee02/bltc3128a843ac2ef28/618d752b6407fe7f991e9915/background-video-d-01.mp4" type="video/mp4" />
             </video>
+            <div className='container lane icons'>
+            <img src={fill} alt='fill' className='fill-icon'/>
+            <img src={top} alt='top'/>
+            <img src={jungle} alt='jungle'/>
+            <img src={mid} alt='mid'/>
+            <img src={bot} alt='bot'/>
+            <img src={support} alt='support'/>
+            </div>
             <div className="container">
                 <div className="scrollable-container">
                     <div className="row text-center align-items-center">
