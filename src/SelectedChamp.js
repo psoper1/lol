079ce66b9518a2ddff88e7d5eaedfbb0b557b8e1
psoper1 <psoper0604@gmail.com>
2023-06-05 20:16:11 -0400

    fixed background video

--- src/SelectedChamp.js
+++ src/SelectedChamp.js
@@ -72,11 +72,11 @@ function SelectedChamp({ champ }) {
   return (
     <div className="container-fluid">
       <Nav />
-      {/* <div className="video-container">
+      <div className=" container video-container">
         <video autoPlay muted loop className="embed-responsive embed-responsive-4by3 background-video">
           <source src="https://assets.contentstack.io/v3/assets/blt2ac872571a60ee02/bltc3128a843ac2ef28/618d752b6407fe7f991e9915/background-video-d-01.mp4" type="video/mp4" />
         </video>
-      </div> */}
+      </div>
       <section className="page-section">
         <div className="container">
           <div className="row">
