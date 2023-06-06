39f75530852f5cec6bbc60364e3dd2184153c357
psoper1 <psoper0604@gmail.com>
2023-06-05 20:59:13 -0400

    fixed some css on the DescSection section

--- src/Components/SelectedChamp/DescSection.js
+++ src/Components/SelectedChamp/DescSection.js
@@ -4,7 +4,7 @@ function DescSection({ champ }) {
             <div className="container desc-section">
                 <h1 className="champ-title-desc">{champ.name.toUpperCase()}, {champ.title.toUpperCase()}</h1>
                 <div className="line-break"></div>
-                <h3 className="champ-blurb-desc fs-2 fs-md-3">{champ.blurb}</h3>
+                <h3 className="champ-blurb-desc fs-md-3">{champ.blurb}</h3>
             </div>
         </>
     )
